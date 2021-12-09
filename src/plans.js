1109965d0490a9a6cd87b76f794ce978aa732c82
Andrea Valgeirsdóttir <andreavalgeirsdottir@Andreas-MacBook-Pro-2.local>
2021-12-09 10:52:12 +0100

    commit

--- src/plans.js
+++ src/plans.js
@@ -2,31 +2,56 @@
 
 window.addEventListener("DOMContentLoaded", start);
 
+// function start() {
+//   document.querySelectorAll(".submenu_item").forEach((item) => {
+//     item.addEventListener("click", toggleItem);
+//   });
+// }
+
+// function toggleItem() {
+//   document.querySelectorAll(".submenu_item").forEach((item) => {
+//     item.addEventListener("click", () => {
+//       console.log("add disabled click");
+//       item.classList.add("disabled");
+//     });
+//   });
+
+//   this.classList.remove("disabled");
+
+//   document.querySelectorAll(".submenu_item").forEach(item => {
+//       if (item.classList.contains("disabled")){
+//           document.querySelectorAll(`.${item.dataset.course}`).forEach(listItem=>{
+//               listItem.classList.add("hidden")
+//           })
+//       } else {
+//           document.querySelectorAll(`.${item.dataset.course}`).forEach(listItem=>{
+//               listItem.classList.remove("hidden")
+//           })
+//       }
+//   });
+// }
+
 function start() {
-  document.querySelectorAll(".submenu_item").forEach((item) => {
-    item.addEventListener("click", toggleItem);
-  });
-}
+  let allButtons = document.querySelectorAll("#plans .submenu_item");
+  console.log("allButtons", allButtons);
+  // let allTabs = document.querySelectorAll(".tab");
+  // console.log("allTabs", allTabs);
 
-function toggleItem() {
-  document.querySelectorAll(".submenu_item").forEach((item) => {
-    item.addEventListener("click", () => {
-      console.log("add disabled click");
-      item.classList.add("disabled");
+  for (let i = 0; i < allButtons.length; i++) {
+    allButtons[i].addEventListener("click", function () {
+      allButtons.forEach((button) => {
+        console.log("submenu button click");
+        button.classList.add("disabled");
+      });
+
+      console.log("this", this);
+      if (this === allButtons[0]) {
+        this.classList.remove("disabled");
+      } else if (this === allButtons[1]) {
+        this.classList.remove("disabled");
+      } else if (this === allButtons[2]) {
+        this.classList.remove("disabled");
+      }
     });
-  });
-
-  this.classList.remove("disabled");
-
-  // document.querySelectorAll(".submenu_item").forEach(item => {
-  //     if (item.classList.contains("disabled")){
-  //         document.querySelectorAll(`.${item.dataset.course}`).forEach(listItem=>{
-  //             listItem.classList.add("hidden")
-  //         })
-  //     } else {
-  //         document.querySelectorAll(`.${item.dataset.course}`).forEach(listItem=>{
-  //             listItem.classList.remove("hidden")
-  //         })
-  //     }
-  // });
+  }
 }
