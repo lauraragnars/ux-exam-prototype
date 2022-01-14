3b4c1d70a6e93c6a50cef0ad35c3e1639ae55faa
Andrea Valgeirsdóttir <andreavalgeirsdottir@Andreas-MBP-2.lan>
2022-01-14 06:16:28 +0000

    blablabl

--- src/index.js
+++ src/index.js
@@ -63,25 +63,3 @@ function openEditProfile() {
     document.querySelector(".edit_profile_container").style.display = "none";
   });
 }
-
-const student = document.getElementById("student");
-const teacher = document.getElementById("teacher");
-const studLog = document.getElementById("studentLogin");
-const teachLog = document.getElementById("teacherLogin");
-const divStud = document.getElementById("login_student");
-const divTeach = document.getElementById("login_teacher");
-student.addEventListener("click", toggleLogin);
-teacher.addEventListener("click", toggleLogin);
-function toggleLogin() {
-  if (studLog.classList.contains("hidden")) {
-    studLog.classList.remove("hidden");
-    teachLog.classList.add("hidden");
-    divStud.classList.add("active");
-    divTeach.classList.remove("active");
-  } else if (teachLog.classList.contains("hidden")) {
-    studLog.classList.add("hidden");
-    teachLog.classList.remove("hidden");
-    divTeach.classList.add("active");
-    divStud.classList.remove("active");
-  }
-}
